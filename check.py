9b3c0506b5ad79a77e9e41dcf07295bb1afff5fb
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2022-02-19 15:24:14 +0100

    Update check.py

--- check.py
+++ check.py
@@ -20,52 +20,74 @@ python3 check.py https
 def Type():
     try:
         protocol = sys.argv[1]
-        if protocol == "http":
-            return protocol
         if protocol == "https":
             return protocol
-        if protocol == "HTTPS":
-            return "https"
+        if protocol == "http":
+            return "http"
         if protocol == "HTTP":
             return "http"
+        if protocol == "HTTPS":
+            return "https"
         else:
-            sys.exit("Invalid protocol type! Only http or https")
+            sys.exit("Invalid protocol type! Only https and http")
     except:
-        sys.exit("Usage examples:\npython3 check.py http\npython3 check.py https")
+        sys.exit("Usage examples:\npython3 check.py https\npython3 check.py http")
 
-socket.timeout(200)
+socket.timeout(20)
 
-def ReadProxies(*, file):
-    with open(f"{file}","r")as f:
-        filedata = f.read()
-        filedatalist = filedata.split()
-    return filedatalist
+def Hosts():
+    try:
+        global fucker
+        fucker = {}
 
+        with open(f"check.txt","r")as f:
+            filedata = f.read()
+            filedatalist = filedata.split("\n")
+        global hosts
+        hosts = {}
+        for i in filedatalist:
+            host, port = i.split(":")
+            hosts[host] = port
+        return hosts
+    except:
+        sys.exit("\033[31mRemove all white space newlines in check.txt\033[0m")
 
 def start():
-    proxytype = Type() 
-    with open(f"check.txt","r")as f:
-        filelenght = len(f.readlines())
-    print(f"\033[0mChecking {filelenght} proxies\n")
+    proxytype = Type()
+    proxiesz3ntl3 = Hosts()
 
-    objectproxies = ReadProxies(file='check.txt')
-    for proxiesd in objectproxies:
+    if proxytype == "http":
         try:
-            r = requests.get('https://httpbin.org/ip', proxies={f'{proxytype}' : f'{proxiesd}'},timeout=200)
-            data = json.loads(r.text)   
-            if data['origin'] == proxiesd:
-                print("\033[32mGood Proxy: \033[0m", proxiesd)
-                with open("goods.txt","a+")as w:
-                    w.write(f"{proxiesd}\n")
-                pass
-            else:
+            for host,port in proxiesz3ntl3.items():
+                print(f"\033[36mChecking Proxy: \033[0m{host}:{port}")  
+                r = requests.get('http://ip-api.com/json', proxies={f'http' : f'{host}:{port}'},timeout=20)
+
+                data = json.loads(r.text) 
                 
-                print("\033[31mBad Proxy \033[0m",proxiesd)
+                if data['query'] == host:
+                    print(f"\033[32mGood Proxy: \033[0m{host}:{port}")
+                    with open("goods.txt","a+")as w:
+                        w.write(f"{host}:{port}\n")
+                else:   
+                    print(f"\033[31mBad Proxy: \033[0m {host}:{port}")
         except:
-            print("\033[31mBad Proxy \033[0m",proxiesd)
+            print(f"\033[31mBad Proxy: \033[0m {host}:{port}")
+    if proxytype == "https":
+        try:
+            for host,port in proxiesz3ntl3.items():
+                print(f"\033[36mChecking Proxy: \033[0m{host}:{port}")  
+                r = requests.get('https://httpbin.org/ip', proxies={f'https' : f'{host}:{port}'},timeout=5)
 
+                data = json.loads(r.text) 
+                
+                if data['origin'] == host:
+                    print(f"\033[32mGood Proxy: \033[0m{host}:{port}")
+                    with open("goods.txt","a+")as w:
+                        w.write(f"{host}:{port}\n")
+                else:   
+                    print(f"\033[31mBad Proxy: \033[0m {host}:{port}")
+        except:
+            print(f"\033[31mBad Proxy: \033[0m {host}:{port}")
 if __name__ == '__main__':
-    thread = Process(target=start(), name="Checking Thread")
-    thread.start()
-    thread.join()
-
+    print("Made by Z3NTL3")
+    start()
